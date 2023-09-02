65d9e6d6d4fc4cb29bdeb9b5e3a32226f0d489f8
davidpd7 <david.5697.9@gmail.com>
2023-09-02 14:57:12 +0100

    Update game.py

--- kate/game.py
+++ kate/game.py
@@ -4,23 +4,29 @@ import os
 
 class Game():
 
-    screen_weight = 720
-    screen_height = 480
-    game_caption = "Actividad"  
-    icon_path = ['images', 'icono.png']
-    icon = pygame.image.load(os.path.join(*icon_path))
-    background_path = ['images', 'fondo.png']
-    background = pygame.image.load(os.path.join(*background_path))
-    music_path = ['music', 'ambiente.ogg']
-    music = pygame.mixer.music.load (os.path.join(*music_path))
+    __screen_weight = 720
+    __screen_height = 480
+    __game_caption = "Actividad"  
+    __icon_path = ['images', 'icono.png']
+    __icon = pygame.image.load(os.path.join(*__icon_path))
+    __background_path = ['images', 'fondo.png']
+    __background = pygame.image.load(os.path.join(*__background_path))
+    __music_path = ['music', 'ambiente.ogg']
     player = Hero()
 
     def __init__(self) -> None:
 
         pygame.init()
+<<<<<<< Updated upstream
         self.screen = pygame.display.set_mode((Game.screen_weight,Game.screen_height))
         pygame.display.set_caption(Game.game_caption)
         pygame.display.set_icon(Game.icon)
+=======
+        self.screen = pygame.display.set_mode((Game.__screen_weight,Game.__screen_height))
+        pygame.display.set_caption(Game.__game_caption)
+        pygame.mixer.music.load (os.path.join(*Game.__music_path))
+        pygame.display.set_icon(Game.__icon)
+>>>>>>> Stashed changes
         self.quit = False
         pygame.mixer.music.play(-1)
         self.fps = 18
@@ -32,7 +38,11 @@ class Game():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit = True
+<<<<<<< Updated upstream
             self.__render()
+=======
+            self.render()
+>>>>>>> Stashed changes
             self.player.handle_event(event)
             self.clock.tick(self.fps)
         self.__quit_game()
@@ -43,6 +53,17 @@ class Game():
         self.screen.blit(Game.player.image, Game.player.rect)
         pygame.display.flip()
 
+<<<<<<< Updated upstream
     def __quit_game(self):
+=======
+    def render(self):
+        self.screen.blit(Game.__background,(0,0))
+        self.screen.blit(Game.player.image, Game.player.rect)
+        pygame.display.flip()
+
+    def quit_game(self):
+                
+        pygame.quit()
+>>>>>>> Stashed changes
 
         pygame.quit()
